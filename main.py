8fba0d52e7bbf97615adab180b93e358cbcb31d5
Dan670 <93826646+Dan670@users.noreply.github.com>
2023-06-03 18:09:48 +0500

    ....

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import random
 
 
 # class Human:
+<<<<<<< Updated upstream
 #     def __init__(self, name="Human"):
 #         self.name = name
 #
@@ -21,6 +22,27 @@ import random
 #                 print(passenger.name)
 #         else:
 #             print(f"There are no passengers in {self.brand}")
+=======
+# def __init__(self, name="Human"):
+# self.name = name
+#
+#
+# class Auto:
+# def __init__(self, brand):
+# self.brand = brand
+# self.passengers = []
+#
+# def add_passenger(self, human):
+# self.passengers.append(human)
+#
+# def print_passengers_names(self):
+# if self.passengers != []:
+# print(f"Names of {self.brand} passengers: ")
+# for passenger in self.passengers:
+# print(passenger.name)
+# else:
+# print(f"There are no passengers in {self.brand}")
+>>>>>>> Stashed changes
 #
 # vasya = Human("Vasya")
 # petya = Human("Petya")
@@ -35,6 +57,7 @@ import random
 # ____________________SIMS__________________________
 
 class Human:
+<<<<<<< Updated upstream
     def __init__(self, name="Human", job=None, home=None, car=None):
         self.name = name
         self.money = 100
@@ -189,10 +212,169 @@ class Human:
         elif dice == 4:
             print("Time to treats!")
             self.shopping(manage='delicacies')
+=======
+def __init__(self, name="Human", job=None, home=None, car=None):
+self.name = name
+self.money = 100
+self.gladness = 50
+self.satiety = 50
+self.job = job
+self.car = car
+self.home = home
+
+def get_home(self):
+self.home = House()
+
+def get_car(self):
+self.car = Auto(brands_of_car)
+
+def get_job(self):
+if self.car.drive():
+pass
+else:
+self.to_repair()
+return
+self.job = Job(job_list)
+
+def eat(self):
+if self.home.food <= 0:
+self.shopping("food")
+else:
+if self.satiety >= 100:
+self.satiety = 100
+return
+self.satiety += 5
+self.home.food -= 5
+
+def work(self):
+if self.car.drive():
+pass
+else:
+if self.car.fuel < 20:
+self.shopping("fuel")
+return
+else:
+self.to_repair()
+return
+self.money += self.job.salary
+self.gladness -= self.job.gladness_less
+self.satiety -= 4
+
+def shopping(self, manage):
+if self.car.drive():
+pass
+else:
+if self.car.fuel < 20:
+manage = "fuel"
+else:
+self.to_repair()
+return
+
+if manage == 'fuel':
+print("I bought fuel")
+self.money -= 100
+self.car.fuel += 100
+elif manage == 'food':
+print("I bought food")
+self.money -= 50
+self.home.food += 50
+elif manage == 'delicacies':
+print("Yay! Delicious!")
+self.gladness += 10
+self.satiety += 2
+self.money -= 15
+
+def chill(self):
+self.gladness += 10
+self.home.mess += 5
+
+def clean_home(self):
+self.gladness -= 5
+self.home.mess += 0
+
+def to_repair(self):
+self.car.strength += 100
+self.money -= 50
+
+def days_indexes(self,day):
+day = f"Today {day} of {self.name}'s life"
+print(f"{day:^50}","\n")
+human_indexes = self.name + "`s indexes"
+print(f"{human_indexes:^50}","\n")
+print(f"Money - {self.money}")
+print(f"Satiety - {self.satiety}")
+print(f"Gladness - {self.gladness}")
+home_indexes = "Home indexes"
+print(f"{home_indexes:^50}","\n")
+print(f"Food - {self.home.food}")
+print(f"Mess - {self.home.mess}")
+car_indexes = f"{self.car.brand} car indexes"
+print(f"{car_indexes:^50}", "\n")
+print(f"Fuel - {self.car.fuel}")
+print(f"Strength - {self.car.strength}")
+
+def is_alive(self):
+if self.gladness < 0:
+print("Depression...")
+return False
+if self.satiety < 0:
+print("Dead..")
+return False
+if self.money <= 500:
+print("Bankrupt...")
+return False
+
+def life(self,day):
+if self.is_alive() == False:
+return False
+if self.home is None:
+print("Settled in the house")
+self.get_home()
+if self.car is None:
+self.get_car()
+print(f"I bought a car {self.car.brand}")
+if self.job is None:
+self.get_job()
+print(f"I don't have a job,going to get a job{self.job.job} with salary {self.job.salary}")
+self.days_indexes(day)
+dice = random.randint(1,4)
+
+if self.satiety < 20:
+print("I'll go eat")
+self.eat()
+elif self.gladness < 20:
+if self.home.mess > 15:
+print("I want to chill,but there is so much mess... \n So I will clean the house")
+self.clean_home()
+else:
+print("Let's chill!")
+self.chill()
+elif self.money < 0:
+print("Start working")
+self.work()
+elif self.car.straingth < 15:
+print("I need to repair my car")
+self.to_repair()
+elif dice == 1:
+print("Let's chill!")
+self.chill()
+elif dice == 2:
+print("Start working")
+self.work()
+elif dice == 3:
+print("Cleaning time")
+self.clean_home()
+elif dice == 4:
+print("Time to treats!")
+self.shopping(manage='delicacies')
+
+
+>>>>>>> Stashed changes
 
 
 class Auto:
     def __init__(self, brand_list):
+<<<<<<< Updated upstream
         self.brand = random.choice(list(brand_list))
         self.fuel = brand_list[self.brand]["fuel"]
         self.strength = brand_list[self.brand]["strength"]
@@ -206,6 +388,21 @@ class Auto:
         else:
             print("The car cannot move")
             return False
+=======
+        self.brand = random.choice(list(brand_list)),
+        self.fuel = brand_list[self.brand]["fuel"],
+        self.strength = brand_list[self.brand]["strength"],
+        self.consumption = brand_list(self.brand] ["consumption"],
+
+        def drive(self):
+            if self.strength > 0 and self.fuel >= self.consumption:
+                self.fuel -= self.consumption
+                self.strength -= 1
+                return True
+            else:
+                print("The car cannot move")
+                return False
+>>>>>>> Stashed changes
 
 
 class House:
@@ -213,13 +410,22 @@ class House:
         self.mess = 0
         self.food = 0
 
+<<<<<<< Updated upstream
 
 job_list = {
+=======
+job_list = {
+}
+>>>>>>> Stashed changes
     "Java developer": {"salary": 50, "gladness_less": 10},
     "Python developer": {"salary": 40, "gladness_less": 3},
     "C++ developer": {"salary": 45, "gladness_less": 25},
     "Rust developer": {"salary": 70, "gladness_less": 1},
 }
+<<<<<<< Updated upstream
+=======
+
+>>>>>>> Stashed changes
 brands_of_car = {
     "BMW": {"fuel": 100, "strength": 100, "consumption": 6},
     "Lada": {"fuel": 50, "strength": 40, "consumption": 10},
@@ -227,6 +433,7 @@ brands_of_car = {
     "Ferrari": {"fuel": 80, "strength": 120, "consumption": 14},
 }
 
+<<<<<<< Updated upstream
 
 class Job:
     def __init__(self, job_list):
@@ -240,3 +447,7 @@ vasya = Human(name="Vasya")
 for day in range(1, 20):
     if vasya.life(day) == False:
         break
+=======
+class Job:
+pass
+>>>>>>> Stashed changes
