4ebded6da3887083c1690d5b43f0d8be91def8a8
Dan670 <93826646+Dan670@users.noreply.github.com>
2023-06-09 15:51:58 +0500

    Update main.py

--- main.py
+++ main.py
@@ -17,18 +17,35 @@ import requests
 #response = requests. get ("https: //httpbin.org/get")
 #print (response.content)
 
+#coin_list = []
+
+#response = requests.get("https://coinmarketcap.com/")
+#print(response.text)
+#response_text = response.text
+
+#response_parse = response_text.split("<span>")
+#for parse_elem1 in response_parse:
+    #if parse_elem1.startswith("$"):
+        #for parse_elem2 in parse_elem1.split("</span>"):
+            #if parse_elem1.startswith("$") and  parse_elem2[1].isdigit():
+                #coin_list.append(parse_elem2)
+
+#btc = coin_list[0]
+#print("BTC =", btc)
+
 coin_list = []
 
-response = requests.get("https://coinmarketcap.com/")
-#print(response.text)
+response = requests.get("https://bank.gov.ua/")
 response_text = response.text
 
 response_parse = response_text.split("<span>")
-for parse_elem1 in response_parse:
-    if parse_elem1.startswith("$"):
-        for parse_elem2 in parse_elem1.split("</span>"):
-            if parse_elem1.startswith("$") and  parse_elem2[1].isdigit():
-                coin_list.append(parse_elem2)
 
-btc = coin_list[0]
-print("BTC =", btc)
+for parse_elem1 in response_parse:
+    if not parse_elem1.startswith("₴"):
+        continue
+    for parse_elem2 in parse_elem1.split("</span>"):
+        if parse_elem2.startswith("₴") and parse_elem2[1].isdigit():
+            coin_list.append(parse_elem2)
+
+hrivnya = coin_list[0]
+print("hrivnya =", hrivnya)
