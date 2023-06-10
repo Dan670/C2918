371ad0e7d01da855ee446b14f795e7a2a374f5a1
Dan670 <93826646+Dan670@users.noreply.github.com>
2023-06-10 18:40:29 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,51 +1,40 @@
 import urllib.request
 import requests
 
-#opener = urllib.build_opener()
-
-#response = opener .open ("https://httpbin.org/get")
-
-#print(response.read()
-
-
-
-#res = requests.post ("https://httpbin.org/post", data-"Test data hello world", headers-("hi": "My title"})
-#print(res.text)
-
-
-
-#response = requests. get ("https: //httpbin.org/get")
-#print (response.content)
-
-#coin_list = []
-
-#response = requests.get("https://coinmarketcap.com/")
-#print(response.text)
-#response_text = response.text
-
-#response_parse = response_text.split("<span>")
-#for parse_elem1 in response_parse:
-    #if parse_elem1.startswith("$"):
-        #for parse_elem2 in parse_elem1.split("</span>"):
-            #if parse_elem1.startswith("$") and  parse_elem2[1].isdigit():
-                #coin_list.append(parse_elem2)
-
-#btc = coin_list[0]
-#print("BTC =", btc)
+response = requests.get("https://coinmarketcap.com/")
+response_text = response.text
 
 coin_list = []
 
-response = requests.get("https://bank.gov.ua/")
-response_text = response.text
-
 response_parse = response_text.split("<span>")
-
 for parse_elem1 in response_parse:
-    if not parse_elem1.startswith("₴"):
-        continue
-    for parse_elem2 in parse_elem1.split("</span>"):
-        if parse_elem2.startswith("₴") and parse_elem2[1].isdigit():
-            coin_list.append(parse_elem2)
+    if parse_elem1.startswith("$"):
+        for parse_elem2 in parse_elem1.split("</span>"):
+            if parse_elem2.startswith("$") and parse_elem2[1].isdigit():
+                coin_list.append(parse_elem2)
 
-hrivnya = coin_list[0]
-print("hrivnya =", hrivnya)
+a = coin_list[0]
+b = coin_list[1]
+c = coin_list[2]
+d = coin_list[3]
+e= coin_list[4]
+f = coin_list[5]
+g = coin_list[6]
+h = coin_list[7]
+i = coin_list[8]
+j = coin_list[9]
+
+
+print('TOP1 =', a)
+print('TOP2 =', b)
+print('TOP3 =', c)
+print('TOP4 =', d)
+print('TOP5 =', e)
+print('TOP6 =', f)
+print('TOP7 =', g)
+print('TOP8 =', h)
+print('TOP9 =', i)
+print('TOP10 =', j)
+
+
+
